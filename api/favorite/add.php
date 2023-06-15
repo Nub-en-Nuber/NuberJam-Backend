b396e130e44e53e88ed4daf1d20217908962cac3
Rama Tri Agung <ramatriagung91@gmail.com>
2023-06-15 20:46:30 +0700

    refactor(favorite) : MP-93 Add condition for test case

--- api/favorite/add.php
+++ api/favorite/add.php
@@ -12,20 +12,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $response = $database->checkToken();
 
     if ($response["status"] == $constant->RESPONSE_STATUS["success"]) {
-        if (!empty($_POST)) {
+        if (!empty($_POST['musicId']) && !empty($_POST['accountId'])) {
             $musicId = $_POST['musicId'];
             $accountId = $_POST['accountId'];
 
-            $query = "INSERT INTO favorite (musicId, accountId) VALUES ('$musicId', '$accountId')";
-            $execute = mysqli_query($database->connection, $query);
-            $check = mysqli_affected_rows($database->connection);
+            $queryCheck = "SELECT * FROM music WHERE musicId = '$musicId'";
+            $execute = mysqli_query($database->connection, $queryCheck);
+            $musicExist = mysqli_num_rows($execute) > 0 ? true : false;
 
-            if ($check > 0) {
-                $response["status"] = $constant->RESPONSE_STATUS["success"];
-                $response["message"] = $constant->RESPONSE_MESSAGES["add_success"];
+            $queryCheck = "SELECT * FROM account WHERE accountId = '$accountId'";
+            $execute = mysqli_query($database->connection, $queryCheck);
+            $accountExist = mysqli_num_rows($execute) > 0 ? true : false;
+
+            if ($musicExist && $accountExist) {
+                $query = "INSERT INTO favorite (musicId, accountId) VALUES ('$musicId', '$accountId')";
+                $execute = mysqli_query($database->connection, $query);
+                $check = mysqli_affected_rows($database->connection);
+
+                if ($check > 0) {
+                    $response["status"] = $constant->RESPONSE_STATUS["success"];
+                    $response["message"] = $constant->RESPONSE_MESSAGES["add_success"];
+                } else {
+                    $response["status"] = $constant->RESPONSE_STATUS["internal_server_error"];
+                    $response["message"] = $constant->RESPONSE_MESSAGES["add_failed"];
+                }
             } else {
-                $response["status"] = $constant->RESPONSE_STATUS["internal_server_error"];
-                $response["message"] = $constant->RESPONSE_MESSAGES["add_failed"];
+                $response["status"] = $constant->RESPONSE_STATUS["bad_request"];
+                $response["message"] = $constant->RESPONSE_MESSAGES["music_or_account_not_exist"];
             }
         } else {
             $response["status"] = $constant->RESPONSE_STATUS["bad_request"];
