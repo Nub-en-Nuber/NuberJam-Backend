ede39493697b53b8b90feedb32635002ed4ec35a
Anjar Harimurti <acool90@yahoo.co.id>
2023-06-17 13:54:09 +0700

    refactor(account) : MP-88 Add condition for account endpoint test case

--- api/account/add.php
+++ api/account/add.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $response = $database->checkToken();
 
     if ($response["status"] == $constant->RESPONSE_STATUS["success"]) {
-        if (!empty($_POST)) {
+        if (!empty($_POST['accountName']) && !empty($_POST['accountUsername']) && !empty($_POST['accountEmail']) && !empty($_POST['accountPassword'])) {
             $accountName = $_POST['accountName'];
             $accountUsername = $_POST['accountUsername'];
             $accountEmail = $_POST['accountEmail'];
@@ -19,16 +19,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $accountPhoto = $constant->BASE_ASSET_URL . "/images/account/default-account.png";
 
             $encryptPassword = Utils::getEncryptPassword($accountPassword);
-            $query = "INSERT INTO account (accountName, accountUsername, accountEmail, accountPassword, accountPhoto) VALUES ('$accountName','$accountUsername','$accountEmail','$encryptPassword','$accountPhoto')";
-            $execute = mysqli_query($database->connection, $query);
-            $check = mysqli_affected_rows($database->connection);
 
-            if ($check > 0) {
-                $response["status"] = $constant->RESPONSE_STATUS["success"];
-                $response["message"] = $constant->RESPONSE_MESSAGES["add_success"];
-            } else {
+            $queryCheck = "SELECT * FROM account WHERE accountUsername = '$accountUsername'";
+            $execute = mysqli_query($database->connection, $queryCheck);
+            $usernameExist = mysqli_num_rows($execute) > 0 ? true : false;
+
+            $queryCheck = "SELECT * FROM account WHERE accountEmail = '$accountEmail'";
+            $execute = mysqli_query($database->connection, $queryCheck);
+            $emailExist = mysqli_num_rows($execute) > 0 ? true : false;
+
+            if ($usernameExist) {
                 $response["status"] = $constant->RESPONSE_STATUS["internal_server_error"];
-                $response["message"] = $constant->RESPONSE_MESSAGES["add_failed"];
+                $response["message"] = $constant->RESPONSE_MESSAGES["username_used"];
+            } else if ($emailExist) {
+                $response["status"] = $constant->RESPONSE_STATUS["internal_server_error"];
+                $response["message"] = $constant->RESPONSE_MESSAGES["email_used"];
+            } else {
+                $query = "INSERT INTO account (accountName, accountUsername, accountEmail, accountPassword, accountPhoto) VALUES ('$accountName','$accountUsername','$accountEmail','$encryptPassword','$accountPhoto')";
+                $execute = mysqli_query($database->connection, $query);
+                $check = mysqli_affected_rows($database->connection);
+
+                if ($check > 0) {
+                    $response["status"] = $constant->RESPONSE_STATUS["success"];
+                    $response["message"] = $constant->RESPONSE_MESSAGES["add_success"];
+                } else {
+                    $response["status"] = $constant->RESPONSE_STATUS["internal_server_error"];
+                    $response["message"] = $constant->RESPONSE_MESSAGES["add_failed"];
+                }
             }
         } else {
             $response["status"] = $constant->RESPONSE_STATUS["bad_request"];
